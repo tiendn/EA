914cb56f50661ba9931fbf53c77736721dda30a5
Tien Dao <20133924@student.hust.edu.vn>
2016-07-19 16:11:47 +0700

    Ver 4.4 - 19/07/2016

--- Js-tut/Exercise/Ex2/data.js
+++ Js-tut/Exercise/Ex2/data.js
@@ -1,12 +1,16 @@
 ﻿$(document).ready(function(){
+
   load();
   var myArr = []; 
   var data = [];
   var count = 0 ;
   var listView = "";
   var maxLength = 0;
-  var countPerLoad = 0;
   var index30 = "";
+  var isFirstLoad = true;
+  const NUMBER_PER_LOAD = 30;
+
+  //  Convert number time into string time.
   function convertTime(time){
     var month = time.slice(4,6);
     var day = time.slice(6,8);
@@ -17,51 +21,75 @@
   function load(){
     $.getJSON('prdata.json', function (result) {
       // Get data.
-      data=result;
+      data = result;
       // Get length
-      maxLength = result.length;
+      console.log(maxLength = result.length);
       // Set first date.
       index30 = data[0].prDateNumeric.toString();
-
       //  Filter 
-      myArr.push(data.filter(checkTime));
-
+      myArr = data.filter(checkTime);
       // Change last date.
-      index30 = myArr[0][29].prDateNumeric.toString();
-      console.log(myArr[0][0].prDateNumeric + "  " + index30);
-
+      if (maxLength >= NUMBER_PER_LOAD)
+        index30 = myArr[NUMBER_PER_LOAD-1].prDateNumeric.toString();
+      else 
+        index30 = myArr[maxLength-1].prDateNumeric.toString();
       // Load into HTML page/
       loadMore();
     });
   }
   // Filter date ..
   function checkTime(dataSm){
-    console.log(index30);
+    // 
     var lastday = convertTime(index30);
+    lastday = lastday.split('/');
+    lastday = new Date(lastday[2],lastday[1],lastday[0]);
+
+    // Filter all days after lastday
     var nextday = convertTime(dataSm.prDateNumeric.toString());
-    // console.log(dataSm.prDateNumeric);
-    return nextday <= lastday;
+    nextday = nextday.split('/');
+    nextday = new Date(nextday[2],nextday[1],nextday[0]);
+    if (isFirstLoad){
+      isFirstLoad = false;
+      return nextday <= lastday;
+    }
+    else 
+      return nextday < lastday;
   }
 
   // Load data into page.
   function loadMore(){
-      for (let i = 0 ; i < 30; i++){
-        var icon = "" ;
-        var time = convertTime(myArr[0][i].prDateNumeric.toString());
+    let length = 0;
+    // If maxLength of array > 30 then make 30 next elements to print 
+    if (count + NUMBER_PER_LOAD <= maxLength){
+      length = NUMBER_PER_LOAD;
+    }
+    // Else take all remain elements.
+    else length = maxLength - count  ;
 
-        if (myArr[0][i].prAttachment === true)    
-          icon = "<img src=\"pr_attachment_icon_iphone_3x.png\" height=\"20px\" width =\"20px\">";
-        listView += "<li class = 'content-list-item'>"+icon+"<b>"+time+"</b><br>"+myArr[0][i].prTitle+"</li>";
-      }
-      $('.content-list').html(listView);
+    // Loop for print to html page
+    for (let i = 0 ; i < length; i++){
+      let icon = "" ;
+      let time = convertTime(myArr[i].prDateNumeric.toString());
+      if (myArr[i].prAttachment === true)    
+        icon = "<img src=\"pr_attachment_icon_iphone_3x.png\" height=\"20px\" width =\"20px\">";
+      listView += "<li class = 'content-list-item'>"+icon+"<b>"+time+"</b><br>"+myArr[i].prTitle+"</li>";
+    }
+    // Count the number of results print into html page
+    count += length;
+    $('.content-list').html(listView);
   }
+
+  // Scroll
   $(window).scroll(function(){
       if ($(window).scrollTop() + $(window).height() == $(document).height() ) {
         if (count < maxLength){
-          // myArr = [];
-          // myArr.push(data.filter(checkTime));
-          // index30 = myArr[0][29].prDateNumeric.toString();
-          // loadMore();
+          myArr = data.filter(checkTime);
+          if (maxLength-count >= NUMBER_PER_LOAD)
+            index30 = myArr[NUMBER_PER_LOAD-1].prDateNumeric.toString();
+          else 
+            index30 = myArr[maxLength-count-1].prDateNumeric.toString();
+          loadMore();
+          console.log(count);
         }
       }
   });
