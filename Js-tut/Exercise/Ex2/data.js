32c48d297772c98215bd7ba06e8c13e598dceaef
Tien Dao <20133924@student.hust.edu.vn>
2016-07-20 09:56:00 +0700

    Ver 4.5.1 - 20/07/2016
    Done Ex2 : filter, grep, prototype, JSON, slice

--- Js-tut/Exercise/Ex2/data.js
+++ Js-tut/Exercise/Ex2/data.js
@@ -1,15 +1,63 @@
 ﻿$(document).ready(function(){
-
   load();
+  Date.prototype.formatTime = function(time) {
+   time = time.toString();
+   var month = time.slice(4,6);
+   var day = time.slice(6,8);
+   var year = time.slice(0,4);
+   switch(Number(day)){
+    case 1 : day += "st";
+            break;
+    case 2 : day += "nd";
+            break;
+    case 1 : day += "rd";
+            break;   
+    default : day += "th";
+            break;                     
+   }
+   switch(Number(month)){
+     case 1 : month = "January";
+             break;
+     case 2 : month = "February";
+             break;
+     case 3 : month = "March";
+             break;        
+     case 4 : month = "April";
+             break;
+     case 5 : month = "May";
+             break;
+     case 6 : month = "June";
+             break;
+     case 7 : month = "July";
+             break;        
+     case 8 : month = "August";
+             break;
+     case 9 : month = "September";
+             break;
+     case 10 : month = "Octorber";
+             break;
+     case 11 : month = "November";
+             break;        
+     case 12 : month = "December";
+             break;               
+   }
+   return day+" "+month+","+year;
+ }
+ //  Convert number time into string time.
+ var date = new Date();
+ console.log(date.formatTime(20160511065716));
+
+
   var myArr = []; 
   var data = [];
   var count = 0 ;
-  var listView = "";
+  
   var maxLength = 0;
   var index30 = "";
   var isFirstLoad = true;
   const NUMBER_PER_LOAD = 30;
 
+  var today = new Date();
   //  Convert number time into string time.
   function convertTime(time){
     var month = time.slice(4,6);
@@ -17,55 +65,47 @@
     var year = time.slice(0,4);
     return day+"/"+month+"/"+year;
   }
-
+  Date.prototype.format = function(time){
+    return convertTime(time);
+  }
   function load(){
     $.getJSON('prdata.json', function (result) {
       // Get data.
       data = result;
       // Get length
-      console.log(maxLength = result.length);
+      maxLength = result.length;
       // Set first date.
-      index30 = data[0].prDateNumeric.toString();
+      index30 = data[0].prDateNumeric;
       //  Filter 
-      myArr = data.filter(checkTime);
+      myArr = jQuery.grep(data,checkTime);
       // Change last date.
       if (maxLength >= NUMBER_PER_LOAD)
-        index30 = myArr[NUMBER_PER_LOAD-1].prDateNumeric.toString();
+        index30 = myArr[NUMBER_PER_LOAD-1].prDateNumeric;
       else 
-        index30 = myArr[maxLength-1].prDateNumeric.toString();
+        index30 = myArr[maxLength-1].prDateNumeric;
       // Load into HTML page/
       loadMore();
     });
   }
   // Filter date ..
-  function checkTime(dataSm){
-    // 
-    var lastday = convertTime(index30);
-    lastday = lastday.split('/');
-    lastday = new Date(lastday[2],lastday[1],lastday[0]);
-
-    // Filter all days after lastday
-    var nextday = convertTime(dataSm.prDateNumeric.toString());
-    nextday = nextday.split('/');
-    nextday = new Date(nextday[2],nextday[1],nextday[0]);
-    if (isFirstLoad){
-      isFirstLoad = false;
-      return nextday <= lastday;
+  function checkTime(n,i){
+    // Get 30 first elements
+    if ( i < 30 ){
+      if (isFirstLoad){
+        isFirstLoad = false;
+        return n.prDateNumeric <= index30;
+      }
+      else 
+        return n.prDateNumeric < index30;
     }
-    else 
-      return nextday < lastday;
+    else
+      return null;
   }
 
   // Load data into page.
   function loadMore(){
-    let length = 0;
-    // If maxLength of array > 30 then make 30 next elements to print 
-    if (count + NUMBER_PER_LOAD <= maxLength){
-      length = NUMBER_PER_LOAD;
-    }
-    // Else take all remain elements.
-    else length = maxLength - count  ;
-
+    let length = myArr.length;
+    var listView = "";
     // Loop for print to html page
     for (let i = 0 ; i < length; i++){
       let icon = "" ;
@@ -76,19 +116,23 @@
     }
     // Count the number of results print into html page
     count += length;
-    $('.content-list').html(listView);
+    $('.content-list').append(listView);
   }
 
   // Scroll
   $(window).scroll(function(){
       if ($(window).scrollTop() + $(window).height() == $(document).height() ) {
         if (count < maxLength){
-          myArr = data.filter(checkTime);
+          // Filter and get only 30 elements
+          myArr = jQuery.grep(data.slice(count,data.length),checkTime);
+          //  Update index30
           if (maxLength-count >= NUMBER_PER_LOAD)
-            index30 = myArr[NUMBER_PER_LOAD-1].prDateNumeric.toString();
+            index30 = myArr[NUMBER_PER_LOAD-1].prDateNumeric;
           else 
-            index30 = myArr[maxLength-count-1].prDateNumeric.toString();
+            index30 = myArr[maxLength-count-1].prDateNumeric;
+          //  Print html page.
           loadMore();
+          //  Count number of results was printed.
           console.log(count);
         }
       }
