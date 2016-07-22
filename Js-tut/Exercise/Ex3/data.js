d584f93c5132a2dec5e51e56f587aa34f07ecb9c
Tien Dao <20133924@student.hust.edu.vn>
2016-07-22 16:26:39 +0700

    Ver 4.7 - 22/07/2016
    Done! Add some effect with search input. Fixed top

--- Js-tut/Exercise/Ex3/data.js
+++ Js-tut/Exercise/Ex3/data.js
@@ -1,13 +1,16 @@
 $(document).ready(function(){
   load();
-  var myArr = []; 
-  var data = [];
+  // init value
   var count = 0 ;
   var lastMonth ="";
   var lastYear ="";
-  var maxLength = 0;
-  var index30 = "";
   var isFirstLoad = true; // If the first load.
+  // 
+  var myArr = []; 
+  var maxLength = 0;
+  var data = [];
+  var onScrollProcess = false;
+  var index30 = "";
   const NUMBER_PER_LOAD = 30;
   var isSearch = false; // If use search function
   var contentSearch = "";
@@ -58,6 +61,7 @@ $(document).ready(function(){
       let currentMonth = getMonth(myArr[i].prDateNumeric);
       let currentYear = getYear(myArr[i].prDateNumeric);
       //  time -> Add new li
+      // Thiếu đối với trường hợp của tk đầu tiên -> Chưa được nhận class do chưa gán giá trị lastyear,month
       if (currentYear != lastYear)
         listViewMore =  "<li class = 'list-group-item list-more'>"+getNameMonth(currentMonth)+" "+currentYear+"</li>";
       else if (currentYear == lastYear){
@@ -96,25 +100,40 @@ $(document).ready(function(){
 
   // Scroll
   $(window).scroll(function(){
-      if ($(window).scrollTop() + $(window).height() == $(document).height() ) {
-        if (count < maxLength){
-          // Filter and get only 30 elements
-          if (!isSearch)
-            myArr = jQuery.grep(data.slice(count,data.length),checkTime);
-          else 
-            myArr = jQuery.grep(contentSearch.slice(count,contentSearch.length),checkTime);
-          //  Update index30
-          if (maxLength-count >= NUMBER_PER_LOAD)
-            index30 = myArr[NUMBER_PER_LOAD-1].prDateNumeric;
-          else 
-            index30 = myArr[maxLength-count-1].prDateNumeric;
-          //  Print html page.
-          loadMore();
-          //  Count number of results was printed.
-          console.log(count);
-        }
+    let value =  Math.abs(($(window).scrollTop() + $(window).height()) - $(document).height());
+    if (value <= 60 && !onScrollProcess){
+      if (count < maxLength){
+        // Filter and get only 30 elements
+        if (!isSearch)
+          myArr = jQuery.grep(data.slice(count,data.length),checkTime);
+        else 
+          myArr = jQuery.grep(contentSearch.slice(count,contentSearch.length),checkTime);
+        //  Update index30
+        if (maxLength-count >= NUMBER_PER_LOAD)
+          index30 = myArr[NUMBER_PER_LOAD-1].prDateNumeric;
+        else 
+          index30 = myArr[maxLength-count-1].prDateNumeric;
+        //  Print html page.
+        onScrollProcess = true;
+        loadMore();
+        onScrollProcess = false;
+        //  Count number of results was printed.
+        console.log(count);
       }
+    }
+    // Scroll search ..
+    if ($(window).scrollTop() >= 50) {
+      $('#searchBox').css("width","100%");
+      $('.search-bar').css("top","0px");
+      $('.search-bar').css("box-shadow","10px 10px 5px #888888");
+    }
+    else {
+      $('#searchBox').css("width","30%");
+      $('.search-bar').css("box-shadow","0px 0px 0px ");
+      $('.search-bar').css("top","5px");
+    }
   });
+  // Reinit some values
   function reinit(){
     count = 0;
     lastYear="";
@@ -131,37 +150,48 @@ $(document).ready(function(){
   $( "#searchBox" ).keypress(function(e) {
     if(e.keyCode === 13)
     {
-      contentSearch = e.currentTarget.value;
-      contentSearch = contentSearch.toLowerCase();
-      // Fitler
-      var searchResults = data.filter(search);
-      if (searchResults.length === 0) {
-       // If users not typed anything at here or this value is not existed on data
-        alert(" No result! ");
+      contentSearch = e.currentTarget.value.toLowerCase();
+      // Check length of contentSearch
+      if (contentSearch.length == 0 ) {
         isSearch = false;
-        // Reinit value
         reinit();
         $('.content-list').html("");
         load();
       }
       else {
-        // Reinit value
-        reinit();
-        maxLength = searchResults.length;
-        isSearch = true;
-      // Set first date.
-        index30 = searchResults[0].prDateNumeric;
-        //  Filter 
-        myArr = jQuery.grep(searchResults,checkTime);
-        // Change last date.
-        if (maxLength >= NUMBER_PER_LOAD)
-          index30 = myArr[NUMBER_PER_LOAD-1].prDateNumeric;
-        else 
-          index30 = myArr[maxLength-1].prDateNumeric;
-        // Load into HTML page/
-        $('.content-list').html("Search result");
-        loadMore();
+        // Fitler
+        var searchResults = data.filter(search);
+        if (searchResults.length === 0) {
+         // If users not typed anything at here or this value is not existed on data
+          alert(" No result! ");
+          isSearch = false;
+          // Reinit value
+          reinit();
+          $('.content-list').html("OOPs! No data available!");
+          // load();
+        }
+        else { // ContentSearch != 0 and have results
+          // Reinit value
+          reinit();
+          maxLength = searchResults.length;
+          // Don't need this line, because when enter, 
+          // this search engine will be started without isSearch variable
+          isSearch = true;
+        // Set first date.
+          index30 = searchResults[0].prDateNumeric;
+          //  Filter 
+          myArr = jQuery.grep(searchResults,checkTime);
+          // Change last date.
+          if (maxLength >= NUMBER_PER_LOAD)
+            index30 = myArr[NUMBER_PER_LOAD-1].prDateNumeric;
+          else 
+            index30 = myArr[maxLength-1].prDateNumeric;
+          // Load into HTML page/
+          $('.content-list').html("Search result");
+          loadMore();
+        }
       }
+      
     }
   });
 
