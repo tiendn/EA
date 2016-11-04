26a552011dca2320939c8ec4836e5d1977713ab4
Tien Dao <20133924@student.hust.edu.vn>
2016-11-04 09:28:20 +0700

    Create Project

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { IonicApp, IonicModule } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { ContactPage } from '../pages/contact/contact';
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    ContactPage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    ContactPage
+  ],
+  providers: []
+})
+export class AppModule {}
